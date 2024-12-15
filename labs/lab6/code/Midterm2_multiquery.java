d170f8af60f7e2cfcb6957deb594ff6f5f93a14f
xzjjasper <xzjjasper@gmail.com>
2024-12-15 14:47:54 -0800

    updates

--- labs/lab6/code/Midterm2_multiquery.java
+++ labs/lab6/code/Midterm2_multiquery.java
@@ -11,30 +11,39 @@ public class Midterm2_multiquery
 		(Connection con=DriverManager.getConnection(url, uid, pw);
 		Statement stmt = con.createStatement(); )
 		{
-			String sql = "SELECT D.dno, dname, COUNT(eno), SUM(salary) FROM emp E JOIN dept D ON E.dno=D.dno"
-							+ " WHERE bdate > '1950-12-01' GROUP BY D.dno, dname ORDER BY D.dno ASC";
+			String sql = "SELECT e.eno, e.ename, count(s.eno) as supervisees, SUM(s.salary) as salaries " +
+					"FROM emp e LEFT OUTER JOIN emp s ON e.eno = s.supereno " +
+					"WHERE e.ename LIKE '%s%' OR e.ename LIKE '%e%' " +
+					"GROUP BY e.eno " +
+					"ORDER BY supervisees DESC, salaries DESC";
 
-			String sql2 = "SELECT eno, ename, salary FROM emp WHERE bdate > '1950-12-01' and dno = ? ORDER BY salary DESC";
+			String sql2 = "SELECT eno, ename, salary, bdate, supereno FROM emp WHERE supereno = ? ORDER BY salary ASC";
 			PreparedStatement pstmt = con.prepareStatement(sql2);
 				
 			ResultSet rst = stmt.executeQuery(sql);
 			// Iterate through list of departments
 			while (rst.next())
 			{
-				String dno = rst.getString(1);
-				String dname = rst.getString(2);
-				System.out.println("Department: " + dno + " Name: " + dname);
-				
-				// Retrieve employees for this department
-				pstmt.setString(1, dno);
-				ResultSet rst2 = pstmt.executeQuery();
-				while (rst2.next())
-					System.out.println(rst2.getString(1)+"\t"+rst2.getString(2)+"\t"+rst2.getString(3));
-					
-				// Print summary line for department
-				System.out.println("Total employees: "+rst.getInt(3)+"\tTotal salary: "+rst.getDouble(4)+"\n");	
-				rst2.close();
+				String supereno = rst.getString(1);
+				int superviseeNo = rst.getInt(3);
+				//print supervisor
+				System.out.println(rst.getString(1) + "\t" + rst.getString(2) + "\t" +
+						rst.getString(3) + "\t" + rst.getDouble(4));
+				System.out.println("Employees supervised:");
+				//print supervisee
+				if (superviseeNo != 0){
+					pstmt.setString(1, supereno);
+					ResultSet rst2 = pstmt.executeQuery();
+					while (rst2.next()){
+						System.out.println(rst2.getString(1) + "\t" + rst2.getString(2) + "\t" +
+								rst2.getString(3) + "\t" + rst2.getString(4) + "\t" + rst2.getString(5));
+					}
+					rst2.close();
+				}
+				System.out.println();
 			}
+			rst.close();
+			pstmt.close();
 		}
 		catch (SQLException ex)
 		{
